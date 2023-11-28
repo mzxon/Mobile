1226a69bbb901178f40cc212cb60add9873a005b
bearjio <mus0906@gmail.com>
2023-11-28 13:01:11 +0900

    Initial Commit

---
+++ test_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_ios
+//
+//  Created by 문지오 on 11/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
